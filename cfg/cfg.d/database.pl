874d6ea86bcbdd05ac23e91123e22138966c3ec4
Timothy Miles-Board <drtjmb@gmail.com>
2014-06-30 17:24:01 +0100

    Initial commit of template for new repositories

---
+++ cfg/cfg.d/database.pl
@@ -0,0 +1,7 @@
+$c->{dbname} = 'template';
+$c->{dbhost} = 'localhost';
+$c->{dbport} = undef;
+$c->{dbsock} = undef;
+$c->{dbuser} = 'template';
+$c->{dbpass} = '3CWdlmu1';
+$c->{dbengine} = 'MyISAM';
